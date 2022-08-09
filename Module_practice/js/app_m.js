b3f8f1a507f9805e84f85cab7604c36419217131
Izzy Irazoque <izzyirazoque@gmail.com>
2022-08-08 18:28:43 -0700

    adding folder structure

--- Module_practice/js/app_m.js
+++ Module_practice/js/app_m.js
@@ -1,7 +1,11 @@
+const tableData = data;
+
+var tbody =d3.select("tbody");
+
 function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
-  
+
     // Next, loop through each object in the data
     // and append a row and cells for each value in the row
     data.forEach((dataRow) => {
@@ -24,17 +28,17 @@ function buildTable(data) {
 
      // Check to see if a date was entered and filter the
   // data using that date.
-  if (date) {
-    // Apply `filter` to the table data to only keep the
-    // rows where the `datetime` value matches the filter value
-    filteredData = filteredData.filter(row => row.datetime === date);
-  };
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    };
 
    // Rebuild the table using the filtered data
   // @NOTE: If no date was entered, then filteredData will
   // just be the original tableData.
-  buildTable(filteredData);
-};
+    buildTable(filteredData);
+}
 
 // Attach an event to listen for the form button
 d3.selectAll("#filter-btn").on("click", handleClick);
